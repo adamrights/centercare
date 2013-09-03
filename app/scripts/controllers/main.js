d437ebbb9e0640feccc8a9dcc8595c0e5a02ec1c
Adam Bedell <adamrights@gmail.com>
2013-09-03 15:47:43 -0400

    Init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('centercareApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
